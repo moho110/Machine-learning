0e9ef58ba0fb7231c64d6b54c86ed81dc5e5cf7c
moho110 <houyue0076@163.com>
2014-10-13 11:11:25 +0800

    helloworld.cpp
    this is my first c++ code

---
+++ helloworld.cpp
@@ -0,0 +1,31 @@
+/*
+* =====================================================================================
+*
+*       Filename:  helloworld.cpp
+*
+*    Description:  helloworld
+*
+*        Version:  1.0
+*        Created:  2014-10-13 11:08:06
+*       Revision:  none
+*       Compiler:  gcc
+*
+*         Author:  VRer
+*        Company:  
+*
+* =====================================================================================
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+   cout<<"hello,world"<<endl;
+   cout<<"press any key when ready..."<<endl;
+   getchar();
+   return 0;
+}
+
+//end of helloworld.cpp
